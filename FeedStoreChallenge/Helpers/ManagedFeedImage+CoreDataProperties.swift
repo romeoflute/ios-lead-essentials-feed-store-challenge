4b2b2d1af4d9cbcc89383acaa64d89838d5283b1
meoflauta <meocodemagnus@gmail.com>
2021-05-22 11:15:38 +0800

    Remove unused function

--- FeedStoreChallenge/Helpers/ManagedFeedImage+CoreDataProperties.swift
+++ FeedStoreChallenge/Helpers/ManagedFeedImage+CoreDataProperties.swift
@@ -11,10 +11,6 @@ import Foundation
 import CoreData
 
 extension ManagedFeedImage {
-	@nonobjc public class func fetchRequest() -> NSFetchRequest<ManagedFeedImage> {
-		return NSFetchRequest<ManagedFeedImage>(entityName: "ManagedFeedImage")
-	}
-
 	@NSManaged public var id: UUID
 	@NSManaged public var imageDescription: String?
 	@NSManaged public var location: String?
