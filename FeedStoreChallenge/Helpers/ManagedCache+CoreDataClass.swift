2bd1dbb3aa2f93f9d06c9328748c92188f637d87
meoflauta <meocodemagnus@gmail.com>
2021-05-22 11:51:29 +0800

    Remove unused functions

--- FeedStoreChallenge/Helpers/ManagedCache+CoreDataClass.swift
+++ FeedStoreChallenge/Helpers/ManagedCache+CoreDataClass.swift
@@ -36,36 +36,3 @@ extension ManagedCache {
 		return ManagedCache(context: context)
 	}
 }
-
-// MARK: Generated accessors for feed
-extension ManagedCache {
-	@objc(insertObject:inFeedAtIndex:)
-	@NSManaged public func insertIntoFeed(_ value: ManagedFeedImage, at idx: Int)
-
-	@objc(removeObjectFromFeedAtIndex:)
-	@NSManaged public func removeFromFeed(at idx: Int)
-
-	@objc(insertFeed:atIndexes:)
-	@NSManaged public func insertIntoFeed(_ values: [ManagedFeedImage], at indexes: NSIndexSet)
-
-	@objc(removeFeedAtIndexes:)
-	@NSManaged public func removeFromFeed(at indexes: NSIndexSet)
-
-	@objc(replaceObjectInFeedAtIndex:withObject:)
-	@NSManaged public func replaceFeed(at idx: Int, with value: ManagedFeedImage)
-
-	@objc(replaceFeedAtIndexes:withFeed:)
-	@NSManaged public func replaceFeed(at indexes: NSIndexSet, with values: [ManagedFeedImage])
-
-	@objc(addFeedObject:)
-	@NSManaged public func addToFeed(_ value: ManagedFeedImage)
-
-	@objc(removeFeedObject:)
-	@NSManaged public func removeFromFeed(_ value: ManagedFeedImage)
-
-	@objc(addFeed:)
-	@NSManaged public func addToFeed(_ values: NSOrderedSet)
-
-	@objc(removeFeed:)
-	@NSManaged public func removeFromFeed(_ values: NSOrderedSet)
-}
